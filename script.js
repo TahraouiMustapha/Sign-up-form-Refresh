9763b816407e4234d00cdc531cd167f5f0facb27
mustapha <moustaphaoussama1818@gmail.com>
2024-11-01 18:05:04 +0100

    just remove console.log from my script

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const submitBtn = document.querySelector('button.create-account');
 
     confirmPasswordInput.addEventListener('keyup', () => {
-        console.log(confirmPasswordInput.value, passwordInput.value)
         if(confirmPasswordInput.value !== passwordInput.value) {
             confirmPasswordInput.style.border = redBorder;
         } else if(confirmPasswordInput.validity.valid) {
